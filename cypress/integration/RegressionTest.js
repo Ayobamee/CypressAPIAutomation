bd73142c34b7cc576a47071f1a80abcca10ffacc
Ayobamee <ayobami.elutade@com>
2022-09-29 13:24:43 +0100

    first commit

--- cypress/integration/RegressionTest.js
+++ cypress/integration/RegressionTest.js
@@ -1,28 +1,51 @@
 /// <reference types= "cypress" />
 
-describe("Quales Course Management Regression Test suite", () => {
-  beforeEach(() => {
-    //Login
-    cy.Login({});
-  });
-
-  context("Tests after login", () => {
-    it("Validate that users with valid credentials can login to the app successfully", () => {
-      //Test successful login
-      cy.checksuccessfulLoginMessage({});
-    });
-
-    it("Validate that courses are displayed successfully", () => {
-      //Test courses are listed.
-      cy.checkCoursesDisplay({});
+describe("Quales Course Management Regression API Test suite", () => {
+  it("Get All Courses", () => {
+    cy.request({
+      method: "GET",
+      url: "/" + "/api/courses",
+    }).as("searchAllCoursesRequest");
+    cy.get("@searchAllCoursesRequest").then((res) => {
+      cy.log(JSON.stringify(res.body));
+      expect(res.status).to.eq(200);
+      expect(res.body.data[0]).to.have.property("id");
+      expect(res.body.data[0]).to.have.property("title");
     });
   });
 
-  context("Tear Down", () => {
-    it("Validate that users can logout of the app successfully", () => {
-      //Test successful logout
-      cy.clickLogout({});
-      cy.assertLoginPage({});
+  it("Get a particular Course", () => {
+    cy.request({
+      method: "GET",
+      url: "/" + "/api/courses/139",
+    }).as("searchACourseRequest");
+    cy.get("@searchACourseRequest").then((res) => {
+      cy.log(JSON.stringify(res.body));
+      expect(res.status).to.eq(200);
+      expect(res.body).to.have.property("id");
+      expect(res.body).to.have.property("title");
+    });
+  });
+
+  it("Create a particular Course", () => {
+    cy.request({
+      method: "POST",
+      url: "/" + "/api/courses/",
+      body: {
+        title: "Cypress Course",
+        categoryId: 2,
+        description: "Testing Cypress course",
+        courseUrl: "https://www.youtube.com/watch?v=8vXoMqWgbQQ",
+        imageUrl: "https://source.unsplash.com/user/c_v_r/900x800",
+        location: "online",
+        isPremium: false,
+      },
+    }).as("createACourseRequest");
+    cy.get("@createACourseRequest").then((res) => {
+      cy.log(JSON.stringify(res.body));
+      expect(res.status).to.eq(200);
+      expect(res.body).to.have.property("id");
+      expect(res.body).to.have.property("title");
     });
   });
 });
